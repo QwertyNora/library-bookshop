f835ec6b79813132698172cf162255c6b8227232
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-03-14 09:46:06 +0100

    Added node_modules and basic structure

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const userRouter = require("./routes/users");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Use imported router for all user requests
+app.use(userRouter);
+
+app.listen(3000, () => console.log("Listening on port:3000"));
