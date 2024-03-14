4219257a7dfdbaf31653e08b7e60202c7b845170
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2024-03-14 14:16:46 +0100

    add new book + validation

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,13 +1,45 @@
 const { Router } = require("express");
 const router = Router();
 
-const { readDatabaseFile } = require("../utils/databaseHelpers");
+const {
+  readDatabaseFile,
+  writeDatabaseFile,
+} = require("../utils/databaseHelpers");
+const { validateBook } = require("../utils/validationHelpers");
 
 const databasePath = "./app/database/books.json";
 
 // CRUD operations
 
 // ----- CREATE -----
+// Create new book:
+router.post("/new-book", async (req, res) => {
+  //Get author from body
+  const newBook = {
+    ...req.body,
+  };
+
+  // validate input fields
+  const [errors, hasErrors] = validateBook(newBook);
+  if (hasErrors) {
+    return res.status(400).json({
+      data: errors,
+    });
+  }
+
+  try {
+    let books = await readDatabaseFile(databasePath);
+    books.push(newBook);
+
+    await writeDatabaseFile(databasePath, books);
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.warn("Error creating author", error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 // ----- READ -----
 // Get all books
